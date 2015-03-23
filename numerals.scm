6c14a0a852ca3a8195c23bd050c7b02711ebd8da
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2015-03-23 12:47:47 +0200

    Реализация на нумерали на Чърч.

---
+++ numerals.scm
@@ -0,0 +1,44 @@
+(define (1+ x) (+ x 1))
+
+(define (repeat n f x)
+  (if ( = n 0) x
+;      (f (repeat (- n 1) f x))))
+      (repeat (- n 1) f (f x))))
+
+(define (c n)
+  (lambda (f)
+    (lambda (x)
+      (repeat n f x))))
+
+(define (print c)
+  ((c 1+) 0))
+
+(define c2 (c 2))
+(define c5 (c 5))
+
+(print c2)
+
+(define c*
+  (lambda (a)
+    (lambda (b)
+      (lambda (f)
+	(lambda (x)
+	  ((a (b f)) x))))))
+
+(print ((c* c2) c5))
+
+(define c+
+  (lambda (a)
+    (lambda (b)
+      (lambda (f)
+	(lambda (x)
+	  ((a f) ((b f) x)))))))
+
+(print ((c+ c2) c5))
+
+;; Типовете не съвпадат!
+;; (define c?
+;;   (lambda (a)
+;;     (lambda (b)
+;;       (lambda (f)
+;; 	((a f) (b f))))))
